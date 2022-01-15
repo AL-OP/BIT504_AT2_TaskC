4d6f01cffce143802c74ce2908ac9f3dae9867fd
AL-OP <96359883+AL-OP@users.noreply.github.com>
2022-01-15 13:11:00 +1300

    Update GameMain.java
    Changed the background and foreground colours of the board to create a 'dark mode' version

--- GameMain.java
+++ GameMain.java
@@ -89,13 +89,13 @@ public class GameMain extends JPanel implements MouseListener{
 	public void paintComponent(Graphics g) {
 		//fill background and set colour to white
 		super.paintComponent(g);
-		setBackground(Color.WHITE);
+		setBackground(Color.BLACK);
 		//ask the game board to paint itself
 		board.paint(g);
 		
 		//set status bar message
 		if (currentState == GameState.Playing) {          
-			statusBar.setForeground(Color.BLACK);          
+			statusBar.setForeground(Color.WHITE);          
 			if (currentPlayer == Player.Cross) {   
 			
 				//displays "X's Turn" on status bar
